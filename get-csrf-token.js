1e34992b22555823a79651efb50cf41355635343
Timothy Janssen <timothy.janssen@sap.com>
2019-01-25 10:06:11 -0800

    Update get-csrf-token.js

--- get-csrf-token.js
+++ get-csrf-token.js
@@ -17,7 +17,7 @@ var options = {
 
 exports.getToken = function(onComplete)  {
 	request(options)
-    .thne( function(data) {console.log('Received token: ' + data.token)})
+    .then( function(data) {console.log('Received token: ' + data.token)})
 	.then( function(data) {
 		onComplete(data.token, data.cookie);
 	});
