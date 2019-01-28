f9e7a18659d6f860b8f22f739d6cb97fac3f12a2
Timothy Janssen <timothy.janssen@sap.com>
2019-01-28 14:04:35 -0800

    Update api.js

--- api.js
+++ api.js
@@ -1,58 +1,56 @@
 var request = require('request-promise');
 
-exports = {
-	call_api_post : function(token, cookie, leadName){
-		var rand = ("0000" +  (Math.random() * 99999)).slice(-5);
-		var payload = { "Name": leadName + " "  + rand,
-						"AccountPartyID": "1001562",
-						"OriginTypeCode": "003",
-						"PriorityCode": "3",
-						"UserStatusCode":"02",
-						"DistributionChannelCode": "01",
-						"DivisionCode": "00",
-						"LeadItem":[{
-							 	"ProductID":"10000954",
-							    "Quantity":"2",
-							    "unitCode": "EA"
-							},	{
-								"ProductID":"10000954",
-							    "Quantity":"2",
-							    "unitCode": "EA"
-							}
-						]};
+exports.call_api_post = function(token, cookie, leadName){
+	var rand = ("0000" +  (Math.random() * 99999)).slice(-5);
+	var payload = { "Name": leadName + " "  + rand,
+					"AccountPartyID": "1001562",
+					"OriginTypeCode": "003",
+					"PriorityCode": "3",
+					"UserStatusCode":"02",
+					"DistributionChannelCode": "01",
+					"DivisionCode": "00",
+					"LeadItem":[{
+						 	"ProductID":"10000954",
+						    "Quantity":"2",
+						    "unitCode": "EA"
+						},	{
+							"ProductID":"10000954",
+						    "Quantity":"2",
+						    "unitCode": "EA"
+						}
+					]};
+
+	var post_options = {
+	    uri:    "https://my341721.crm.ondemand.com/sap/c4c/odata/v1/c4codataapi/LeadCollection",
+	    method:  "POST",
+	    json:    true,
+	    body: payload,
+	    headers: {      
+	         "Authorization": "Basic YWRtaW5pc3RyYXRpb24wMTpXZWxjb21lNQ==", // base64 encoding of administration01:Welcome5
+	         "Content-Type":  "application/json",
+	         "x-csrf-token": token,
+	         "cookie": cookie
+	    }
+	};
+
+	request(post_options)
+	.then(console.log('[POST] Request completed'));
+};
 	
-		var post_options = {
-		    uri:    "https://my341721.crm.ondemand.com/sap/c4c/odata/v1/c4codataapi/LeadCollection",
-		    method:  "POST",
-		    json:    true,
-		    body: payload,
-		    headers: {      
-		         "Authorization": "Basic YWRtaW5pc3RyYXRpb24wMTpXZWxjb21lNQ==", // base64 encoding of administration01:Welcome5
-		         "Content-Type":  "application/json",
-		         "x-csrf-token": token,
-		         "cookie": cookie
-		    }
-		};
-	
-		request(post_options)
-		.then(console.log('[POST] Request completed'));
-	},
-	
-	call_api_get : function(token, cookie){	
-		var get_options = {
-		    uri:    "https://my341721.crm.ondemand.com/sap/c4c/odata/v1/c4codataapi/LeadCollection",
-		    method:  "GET",
-		    json:    true,
-		    headers: {      
-		         "Authorization": "Basic YWRtaW5pc3RyYXRpb24wMTpXZWxjb21lNQ==", // base64 encoding of administration01:Welcome5
-		         "Content-Type":  "application/json",
-		         "x-csrf-token": token,
-		         "cookie": cookie
-		    }
-		};
-	
-		request(get_options)
-		.then(console.log('[GET] Request completed'));
-	}
-}
+exports.call_api_get = function(token, cookie){	
+	var get_options = {
+	    uri:    "https://my341721.crm.ondemand.com/sap/c4c/odata/v1/c4codataapi/LeadCollection",
+	    method:  "GET",
+	    json:    true,
+	    headers: {      
+	         "Authorization": "Basic YWRtaW5pc3RyYXRpb24wMTpXZWxjb21lNQ==", // base64 encoding of administration01:Welcome5
+	         "Content-Type":  "application/json",
+	         "x-csrf-token": token,
+	         "cookie": cookie
+	    }
+	};
+
+	request(get_options)
+	.then(console.log('[GET] Request completed'));
+};
 
