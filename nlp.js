246be3854088b628533933a036251f822d2a51ae
Timothy Janssen <timothy.janssen@sap.com>
2019-01-29 13:57:36 -0800

    Update nlp.js

--- nlp.js
+++ nlp.js
@@ -7,24 +7,24 @@ exports.getSelOpts = function (nlp_obj) {
 	var filter = "";
 	var top = "";
 
-	url = [search, orderby, format, filter, top];
+	var url = [search, orderby, format, filter, top];
 
 	format = "&$format=json";
 
 	var funcs = {
-		sort: function(obj, url) { 
+		sort: function(obj) { 
 			url.orderby = "&$orderby=LastChangeDateTime" + obj.order;
 		},
-		number: function(obj, url) { 
+		number: function(obj) { 
 			url.top = "&$top=" + obj.scalar;
 		},
-		organization: function(obj, url) { 
+		organization: function(obj { 
 			url.filter = add_to_filter(filter, "Company eq " + obj.raw);
 		},
-		datetime: function(obj, url) { 
+		datetime: function(obj) { 
 			url.filter = add_to_filter(filter, "CreationDateTime ge datetimeoffset'" + obj.iso + "'");
 		},
-		default: function(obj, url) {
+		default: function(obj) {
 			//nada
 		}
 	}
@@ -33,7 +33,7 @@ exports.getSelOpts = function (nlp_obj) {
 
 	Object.keys(nlp_obj).forEach( function(key) {
 		var func = funcs[key] || funcs['default'];
-		func(nlp_obj[key], url);
+		func(nlp_obj[key]);
 		console.log(key + " : " + url.join(''))
 	});
 
