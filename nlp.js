3f9a7285a6a9f9b1090d7266b95fabd22012d59c
Timothy Janssen <timothy.janssen@sap.com>
2019-01-29 11:23:56 -0800

    Update nlp.js

--- nlp.js
+++ nlp.js
@@ -1,4 +1,40 @@
 
 exports.getSelOpts = function (nlp_obj) {
-	return '$search=%27API%27&$orderby=LastChangeDateTime%20desc&$format=json';
+
+	var search, orderby, format, filter, top = "";
+
+	format = "&$format=json";
+
+	Object.keys(nlp_obj).forEach( function(key) {
+		switch(key) {
+			case sort:
+				orderby = "&$orderby=LastChangeDateTime" + nlp_obj[key].order;
+				break;
+			case number:
+				top = "&$top=" + nlp_obj[key].scalar;
+				break;
+			case organization:
+				filter = add_to_filter(filter, "Company eq " + nlp_obj[key].raw);
+				break;
+			case location:
+
+				break;
+			case datetime:
+				filter = add_to_filter(filter, "CreationDateTime ge datetimeoffset'" + nlp_obj[key].iso + "'");
+				break;
+			default:
+			// nada
+		}
+	})
+
+	return format + search + orderby + filter + top;
 };
+
+add_to_filter(filter, str){
+	if(filter === "") {
+		filter = "$filter=" + str;
+	} else {
+		filter = filter + " and" + str;
+	}
+	return filter;	
+}
