f47615829e0d3f783de6fbc4f04a677ddc620e0d
Timothy Janssen <timothy.janssen@sap.com>
2019-02-19 16:15:46 -0800

    Update api.js

--- api.js
+++ api.js
@@ -4,6 +4,8 @@ exports.call_api_post = function(token, cookie, lead){
 
 	var date = new Date(lead.lead_date.iso).toISOString();
 
+	console.log("'" + lead.lead_amount.amount + "'");
+
 	var payload = { "Name": lead.lead_name,
 					"AccountPartyID": "1001562",
 					"OriginTypeCode": "003",
