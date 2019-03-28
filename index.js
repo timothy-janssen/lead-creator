e2300aa978f0116b1a9328ddc2520f7d403df07d
Timothy Janssen <timothy.janssen@sap.com>
2019-03-28 11:14:56 -0700

    xx

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ app.post('/create-lead', function (req, res) {
 		"lead_date": memory['lead-date']
 
 	}
-	console.log("Creating lead: " + lead.lead_name.toString());
+	console.log("Creating lead: " + lead.lead_name.stringify());
 
 	csrf.getToken(potential_cookie, potential_token)
 	.then( function(token_data){
