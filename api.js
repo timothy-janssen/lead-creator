3234e76777fdcdff6b1b57f8b5e808a83dbf83f9
Timothy Janssen <timothy.janssen@sap.com>
2019-05-03 11:21:05 -0700

    Update api.js

--- api.js
+++ api.js
@@ -60,7 +60,7 @@ exports.call_api_get = function(token, cookie, sel_opts){
 	    }
 	};
 
-	console.log("[GET] " + get_options.uri);
+	console.log("[GET] " + get_options.headers);
 
 	return request.get(get_options)
 	.then(function (data){
