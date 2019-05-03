80df3e96e351e0c5edc588efeaa2ffbacdb1b4d3
Timothy Janssen <timothy.janssen@sap.com>
2019-05-03 11:12:40 -0700

    Env Vars

--- config.js
+++ config.js
@@ -1,6 +1,9 @@
 module.exports = {
    PORT: process.env.PORT,
-   //BOT_TOKEN: '5aebd90a1d94414494d802532f91fd2b', //Lead bot
-   C4_API_AUTH: "Basic VVNTQUxFU1JFUDAxOldlbGNvbWUx==", // base64 encoding of USSALESREP01:Welcome1
-   BOT_TOKEN: '8181028f4d91e01775d2e44662c4471f'
+   C4_API_AUTH: "Basic " + process.env.C4_API_AUTH // base64 encoding of "<user>:<password>"
+   BOT_TOKEN: process.env.BOT_TOKEN // Hex encoded token (from CAI settings)
 };
+
+//heroku config:set C4_API_AUTH="Basic VVNTQUxFU1JFUDAxOldlbGNvbWUx==" BOT_TOKEN='8181028f4d91e01775d2e44662c4471f' -a lead-creator 
+
+
--- example.config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-   PORT: process.env.PORT,
-   C4_API_AUTH: ,// base64 encoding of "<user>:<password>"
-   BOT_TOKEN: ,//Hex encoded token (from CAI settings)
-};
