8d32973eec3d9027fa394b82db168e77d587cddc
Timothy Janssen <timothy.janssen@sap.com>
2019-10-07 16:42:18 -0700

    Update index.js

--- index.js
+++ index.js
@@ -111,7 +111,17 @@ app.post('/save-lead-name', function (req, res) {
 
 app.get('/wake', function (req, res) {
 	console.log('[GET] wake the app up')
-	res.end();
+
+	//conversation memory
+	const memory = req.body.conversation.memory;
+
+	var potential_cookie = memory['cookie'];
+	var potential_token = memory['token'];
+
+	csrf.getToken(potential_cookie, potential_token)
+	.then( function(){
+		res.end();
+	});	
 });
 
 app.get('/', function (req, res) {
